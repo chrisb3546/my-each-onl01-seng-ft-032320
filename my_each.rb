4ad0b16801e21e7f346fd3b12ed2fa4809dca154
chris brady <chrisb3546@yahoo.com>
2020-03-28 21:10:30 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
-  # code here
+array = ['hi', 'hello', 'bye', 'goodbye']
+
+def my_each (array)# put argument(s) here
+i=0 
+while i < array.length 
+yield (array[i])
+i = i+1
+end
+return array
 end
